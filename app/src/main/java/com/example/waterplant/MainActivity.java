e8bd2da08b87564bffe270632ade3d17cb614d31
DESKTOP-OSFSGV9\Jeppe <mosaren@hotmail.com>
2022-11-05 14:57:01 +0100

    basic working recyclerview testing dimensions

--- app/src/main/java/com/example/waterplant/MainActivity.java
+++ app/src/main/java/com/example/waterplant/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.waterplant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 //Import for basics
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.widget.TextView;
 import android.view.View;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 //----------
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
         MyRecyclerViewAdapter menuRecAdapter = new MyRecyclerViewAdapter(new String[]{"1", "2", "3", "4", "5", "6", "7","8"});
         recyclerView = findViewById(R.id.recyclerView1);
         recyclerView.setAdapter(menuRecAdapter);
+
+        Configuration configuration = getResources().getConfiguration();
+        int screenWidthDp = configuration.screenWidthDp; //The current width of the available screen space, in dp units, corresponding to screen width resource qualifier.
+        int screenHeightDp = configuration.screenHeightDp;
+
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, " W "+screenWidthDp + " H " + screenHeightDp, Toast.LENGTH_LONG);
+        toast.show();
+
+
+
     }
 }
